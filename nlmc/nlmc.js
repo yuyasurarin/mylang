f98226510c636df70e5f165977cd7165e9ebdb49
haruki1234 <haruki070617@gmail.com>
2022-04-18 19:31:42 +0900

    change a little

--- nlmc/nlmc.js
+++ nlmc/nlmc.js
@@ -14,23 +14,33 @@ class NLMC {
         let numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
         let register = {
+            "ceq": 0,
+            "ret": 0,
+
             "rax": 0,
+            "rdx": 0,
             "rdi": 0,
+            "al": 0,
         };
         let label = {};
-        let stack = Array(10).fill(null); // スタックの定義 スタックのサイズ
+        let array = Array(256).fill(null); // スタックの定義 スタックのサイズ
+        let stack = Array(256).fill(null); // スタックの定義 スタックのサイズ
         let stp = 0;
 
         let t = this.tokenaize(c);
         console.log(t);
-        // instructions
         let p = 0;
         while (p<t.length) {
             if (t[p][0]=="label") {
                 label[t[p][1][0]] = p;
             }
-            else if (t[p][0]=="mov") {
-                if (numbers.includes(t[p][1][1])) {
+            p++;
+        }
+        // instructions
+        p = 0;
+        while (p<t.length) {
+            if (t[p][0]=="mov") {
+                if (numbers.includes(t[p][1][1][0])) {
                     register[t[p][1][0]] = Number(t[p][1][1]);
                 }
                 else {
@@ -38,7 +48,7 @@ class NLMC {
                 }
             }
             else if (t[p][0]=="add") {
-                if (numbers.includes(t[p][1][1])) {
+                if (numbers.includes(t[p][1][1][0])) {
                     register[t[p][1][0]] += Number(t[p][1][1]);
                 }
                 else {
@@ -46,7 +56,7 @@ class NLMC {
                 }
             }
             else if (t[p][0]=="sub") {
-                if (numbers.includes(t[p][1][1])) {
+                if (numbers.includes(t[p][1][1][0])) {
                     register[t[p][1][0]] -= Number(t[p][1][1]);
                 }
                 else {
@@ -54,15 +64,23 @@ class NLMC {
                 }
             }
             else if (t[p][0]=="mul") {
-                if (numbers.includes(t[p][1][1])) {
+                if (numbers.includes(t[p][1][1][0])) {
                     register[t[p][1][0]] *= Number(t[p][1][1]);
                 }
                 else {
                     register[t[p][1][0]] *= register[t[p][1][1]];
                 }
             }
+            else if (t[p][0]=="div") {
+                if (numbers.includes(t[p][1][1][0])) {
+                    register[t[p][1][0]] = register[t[p][1][0]]/Number(t[p][1][1]);
+                }
+                else {
+                    register[t[p][1][0]] = register[t[p][1][0]]/register[t[p][1][1]];
+                }
+            }
             else if (t[p][0]=="push") {
-                if (numbers.includes(t[p][1][0])) {
+                if (numbers.includes(t[p][1][0][0])) {
                     stack[stp] = Number(t[p][1][0]);
                 }
                 else {
@@ -75,11 +93,34 @@ class NLMC {
                 register[t[p][1][0]] = Number(stack[stp]);
                 stack[stp] = null
             }
-            //console.log(t[p],register,stack);
+            else if (t[p][0]=="jmp") {
+                p = label[t[p][1][0]];
+            }
+            else if (t[p][0]=="cmp") {
+                let a = register[t[p][1][0]];
+                let b;
+                if (numbers.includes(t[p][1][1][0])) { b = Number(t[p][1][1]); }
+                else { b = register[t[p][1][1]]; }
+                if (a==b) { register["ceq"] = 1; }
+                else { register["ceq"] = 0; }
+            }
+            else if (t[p][0]=="jz") {
+                if (register["ceq"]==1) {
+                    p = label[t[p][1][0]];
+                }
+            }
+            else if (t[p][0]=="jnz") {
+                if (register["ceq"]!=1) {
+                    p = label[t[p][1][0]];
+                }
+            }
+            // console.log(t[p],register,stack);
             p++;
         }
-        console.log("result:",register["rax"]);
+        return register["ret"]
     }
+
+
     tokenaize(c) {
         let token = [];
         let spc = c.split("\n");
