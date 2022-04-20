dfd7c33ce112a6293d202a8f72e081f707d79f5a
haruki1234 <haruki070617@gmail.com>
2022-04-21 06:32:26 +0900

    change a little

---
+++ nlmc/c/nlmc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int code[6][4] = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+
+void run() {
+    int int_var[10];
+    return;
+}
+void main() {
+    printf("a");
+    return;
+}
