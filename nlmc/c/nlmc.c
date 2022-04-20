66d30af689a0a9129dfaa6e627006e433d57efbe
haruki1234 <haruki070617@gmail.com>
2022-04-21 06:43:35 +0900

    change a little

--- nlmc/c/nlmc.c
+++ nlmc/c/nlmc.c
@@ -4,8 +4,12 @@ int code[6][4] = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
 
 void run() {
     int int_var[10];
+    int stack[256];
+    int stp = 0;
+    
     return;
 }
+
 void main() {
     printf("a");
     return;
